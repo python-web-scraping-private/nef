34648de355b7447df27e1bd6dc011c314ea4a4d3
Neffneff <aternalfriendship1006@gmail.com>
2017-01-25 03:59:54 +0900

    first scrape

--- scrapetest.py
+++ scrapetest.py
@@ -15,6 +15,7 @@ def getTitle(url):
         return None
     return title
 
+
 title = getTitle("http://www.pythonscraping.com/pages/page1.html")
 if title == None:
     print("Title could not be found")
